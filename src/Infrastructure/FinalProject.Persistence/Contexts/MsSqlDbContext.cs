40d9a33bd083ed001ed2d7895dbc1de5cf0b7c20
Bedirhan Şık <Bedirhan_98@outlook.com>
2022-06-24 10:12:10 +0300

    Temel Onion Architecture yapısı kuruldu.

---
+++ src/Infrastructure/FinalProject.Persistence/Contexts/MsSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Persistence.Contexts
+{
+    public class MsSqlDbContext : DbContext
+    {
+        public MsSqlDbContext(DbContextOptions<MsSqlDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ src/Presentation/FinalProject.API/Controllers/DenemeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DenemeController : ControllerBase
+    {
+
+
+
+    }
+}
